32465c1506fc99df20e9d9bbb8768d505fdc5c17
ABHIJITH HEGDE <60565384+abhijithhegde@users.noreply.github.com>
2022-12-25 12:24:53 +0530

    delete added

--- app/src/main/java/com/example/firebase_auth/Admin_panel.java
+++ app/src/main/java/com/example/firebase_auth/Admin_panel.java
@@ -24,6 +24,7 @@ public class Admin_panel extends AppCompatActivity {
     ImageView logouthome;
     RecyclerView recyclerView;
     Adaptor adaptor;
+    TextView name;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -31,10 +32,8 @@ public class Admin_panel extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_panel);
         //getSupportActionBar().hide();
-
         recyclerView = (RecyclerView) findViewById(R.id.rv);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
         FirebaseRecyclerOptions<Users> options =
                 new FirebaseRecyclerOptions.Builder<Users>()
                         .setQuery(FirebaseDatabase.getInstance().getReference().child("user"), Users.class)
@@ -43,33 +42,33 @@ public class Admin_panel extends AppCompatActivity {
         recyclerView.setAdapter(adaptor);
 
 
-        logouthome = findViewById(R.id.logouthome);
-        logouthome.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Dialog dialog = new Dialog(Admin_panel.this, R.style.Dialoge);
-                dialog.setContentView(R.layout.dialog_layout);
-                TextView yesbtn, nobtn;
-                yesbtn = dialog.findViewById(R.id.yesbtn);
-                nobtn = dialog.findViewById(R.id.nobtn);
-                yesbtn.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        FirebaseAuth.getInstance().signOut();
-                        startActivity(new Intent(Admin_panel.this, Login_page.class));
-                        finish();
-                    }
-                });
-                nobtn.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        dialog.dismiss();
-
-                    }
-                });
-                dialog.show();
-            }
-        });
+//        logouthome = findViewById(R.id.logouthome);
+//        logouthome.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Dialog dialog = new Dialog(Admin_panel.this, R.style.Dialoge);
+//                dialog.setContentView(R.layout.dialog_layout);
+//                TextView yesbtn, nobtn;
+//                yesbtn = dialog.findViewById(R.id.yesbtn);
+//                nobtn = dialog.findViewById(R.id.nobtn);
+//                yesbtn.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        FirebaseAuth.getInstance().signOut();
+//                        startActivity(new Intent(Admin_panel.this, Login_page.class));
+//                        finish();
+//                    }
+//                });
+//                nobtn.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        dialog.dismiss();
+//
+//                    }
+//                });
+//                dialog.show();
+//            }
+//        });
 
     }
 
@@ -87,9 +86,32 @@ public class Admin_panel extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.search,menu);
-        MenuItem item = menu.findItem(R.id.search);
-        SearchView searchView = (SearchView)item.getActionView();
+        getMenuInflater().inflate(R.menu.search, menu);
+        getMenuInflater().inflate(R.menu.logout,menu);
+        MenuItem item1 = menu.findItem(R.id.search);
+        MenuItem item2 = menu.findItem(R.id.logout);
+        MenuItem item3 = menu.findItem(R.id.update);
+
+        item2.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(Admin_panel.this, Login_page.class));
+                finish();
+                return false;
+            }
+        });
+
+        item3.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                startActivity(new Intent(Admin_panel.this,update_profile.class));
+                finish();
+                return false;
+            }
+        });
+
+        SearchView searchView = (SearchView) item1.getActionView();
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String s) {
@@ -105,13 +127,15 @@ public class Admin_panel extends AppCompatActivity {
         });
         return super.onCreateOptionsMenu(menu);
     }
-    private void txtSearch(String str){
+
+    private void txtSearch(String str) {
         FirebaseRecyclerOptions<Users> options =
                 new FirebaseRecyclerOptions.Builder<Users>()
-                        .setQuery(FirebaseDatabase.getInstance().getReference().child("user").orderByChild("name").startAt(str).endAt(str+"~"), Users.class)
+                        .setQuery(FirebaseDatabase.getInstance().getReference().child("user").orderByChild("name").startAt(str).endAt(str + "~"), Users.class)
                         .build();
         adaptor = new Adaptor(options);
         adaptor.startListening();
         recyclerView.setAdapter(adaptor);
     }
+
 }
